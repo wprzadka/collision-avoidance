81f69690eac9242cd5ac315248922749b28bbe53
Viking <wiktor.przadka.1@gmail.com>
2021-12-30 23:02:59 +0100

    add example model from stable_baselines

---
+++ examples/stable-baselines.py
@@ -0,0 +1,31 @@
+from stable_baselines3 import SAC
+from openai_gym_env.env import CollisionAvoidanceEnv
+from stable_baselines3.common.env_checker import check_env
+
+
+if __name__ == '__main__':
+
+    env = CollisionAvoidanceEnv(5, 3, 10., win_size=(1200, 900))
+    check_env(env)
+
+    model = SAC("MultiInputPolicy", env).learn(total_timesteps=10000)
+    model.save("sac_model")
+
+    print(f'actions: {env.action_space}')
+    print(f'observations: {env.observation_space}')
+
+    episodes_num = 5
+    for _ in range(episodes_num):  # episodes
+        done = False
+        current_total_reward = 0
+        observation = env.reset()
+        t = 0
+        while not done:
+            env.render()
+            action, state = model.predict(observation, deterministic=True)
+            observation, reward, done, _ = env.step(action)
+            current_total_reward += reward
+            t += 1
+
+        print(f'ends after {t} with total reward of {current_total_reward}')
+    env.close()
