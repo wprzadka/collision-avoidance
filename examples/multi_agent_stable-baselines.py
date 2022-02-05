0020ec0420e496ff4cb62dc117d221c68eea1249
Viking <wiktor.przadka.1@gmail.com>
2022-02-05 23:43:47 +0100

    improve multi agent approach

--- examples/multi_agent_stable-baselines.py
+++ examples/multi_agent_stable-baselines.py
@@ -1,34 +1,42 @@
-from stable_baselines3 import SAC
-from openai_gym_env.env import CollisionAvoidanceEnv
+import numpy as np
+from stable_baselines3 import PPO, SAC
+from openai_gym_env.multi_agent_env import MultiAgentCollisionAvoidanceEnv
 from stable_baselines3.common.env_checker import check_env
 import time
 
 
 if __name__ == '__main__':
 
-    env = CollisionAvoidanceEnv(10, 3, 10., win_size=(600, 400), algorithm='ORCA')
-    check_env(env)
+    env = MultiAgentCollisionAvoidanceEnv(
+        agents_num=3,
+        visible_agents_num=2,
+        max_speed=10.,
+        win_size=(600, 400)
+    )
 
-    model = SAC.load('sac_model.zip', env)
-    # model.learn(total_timesteps=10)
-    # model.save('sac_model')
+    # model = SAC(env=env, policy='MultiInputPolicy')
+    model = SAC.load('multi_sac_model.zip', env)
+    # model.learn(total_timesteps=1000)
+    # model.save('multi_sac_model')
 
     print(f'actions: {env.action_space}')
     print(f'observations: {env.observation_space}')
 
-    episodes_num = 5
+    episodes_num = 20
     for _ in range(episodes_num):  # episodes
         done = False
-        current_total_reward = 0
+        current_total_reward = np.zeros(shape=env.agents_num)
         observation = env.reset()
         t = 0
         while not done:
             env.render()
-            action, state = model.predict(observation, deterministic=True)
-            observation, reward, done, _ = env.step(action)
-            current_total_reward += reward
-            t += 1
+
+            for i in range(env.agents_num):
+                action, _ = model.predict(observation=observation, deterministic=True)
+                observations, reward, done, _ = env.step(action)
+                current_total_reward[i] += reward
             time.sleep(0.05)
+            t += 1
 
         print(f'ends after {t} with total reward of {current_total_reward}')
     env.close()
--- openai_gym_env/multi_agent_env.py
+++ openai_gym_env/multi_agent_env.py
@@ -1,19 +1,16 @@
 from abc import ABC
 
-import gym
-from gym import spaces
 import numpy as np
 import pygame as pg
+from gym import spaces
+import gym
 
 from environment.simulation import Simulation
 from environment.agents import Agents
-from VO.ReciprocalVelocityObstacle import ReciprocalVelocityObstacle
-from ORCA.OptimalReciprocalColisionAvoidance import ORCA
 
 
-class CollisionAvoidanceEnv(gym.Env, ABC):
+class MultiAgentCollisionAvoidanceEnv(gym.Env, ABC):
     metadata = {'render.modes': ['human']}
-    available_algorithms = [None, 'VO', 'RVO', 'ORCA']
     collision_penalty = 100.
     out_of_bounds_penalty = 10_000.
     distance_reward = 20.
@@ -25,16 +22,21 @@ class CollisionAvoidanceEnv(gym.Env, ABC):
             visible_agents_num: int,
             max_speed: float,
             win_size: tuple = (1200, 900),
-            algorithm: str = None,
             distance_quantification: int = 5,
             time_step: float = 0.25,
             time_limit: int = 100
     ):
-        super(CollisionAvoidanceEnv, self).__init__()
+        super(MultiAgentCollisionAvoidanceEnv, self).__init__()
 
         self.max_agents_speed = max_speed
         self.agents_num = agents_num
         self.visible_agents_num = visible_agents_num
+
+        self.curr_agent_action = 0
+        self.next_step_velocities = np.empty(shape=(agents_num, 2))
+        self.dones = np.full(shape=self.agents_num, fill_value=False, dtype=bool)
+        self.rewards = np.zeros(shape=self.agents_num)
+
         self.win_size = win_size
 
         self.action_space = spaces.Box(
@@ -45,26 +47,31 @@ class CollisionAvoidanceEnv(gym.Env, ABC):
 
         self.observation_space = spaces.Dict({
             'velocity_x': spaces.Box(
-                shape=(visible_agents_num,),
+                shape=(self.visible_agents_num,),
                 low=-max_speed,
                 high=max_speed
             ),
             'velocity_y': spaces.Box(
-                shape=(visible_agents_num,),
+                shape=(self.visible_agents_num,),
                 low=-max_speed,
                 high=max_speed
             ),
             'position_x': spaces.Box(
-                shape=(visible_agents_num,),
+                shape=(self.visible_agents_num,),
                 low=0,
                 high=self.win_size[0]
             ),
             'position_y': spaces.Box(
-                shape=(visible_agents_num,),
+                shape=(self.visible_agents_num,),
                 low=0,
                 high=self.win_size[1]
             ),
             'target': spaces.Box(
+                shape=(2,),
+                low=np.zeros(2),
+                high=np.array(self.win_size)
+            ),
+            'self_position': spaces.Box(
                 shape=(2,),
                 low=np.zeros(2),
                 high=np.array(self.win_size)
@@ -84,84 +91,70 @@ class CollisionAvoidanceEnv(gym.Env, ABC):
         self.algorithm = None
         self.get_velocities = None
 
-        if algorithm is None:
-            self.get_velocities = self.simulation.agents.get_preferred_velocities
-        elif algorithm in ['VO', 'RVO']:
-            self.algorithm = ReciprocalVelocityObstacle(
-                agents_num=self.agents_num,
-                visible_agents_num=visible_agents_num,
-                reciprocal=algorithm == 'RVO'
-            )
-            self.get_velocities = lambda: self.algorithm.compute_velocities(self.simulation.agents)
-        elif algorithm == 'ORCA':
-            self.algorithm = ORCA(
-                agents_num=self.agents_num,
-                visible_agents_num=visible_agents_num,
-                time_step=time_step,
-                time_horizon=20 * time_step
-            )
-            self.get_velocities = lambda: self.algorithm.compute_velocities(self.simulation.agents)
-        else:
-            raise Exception(f'{algorithm} is not in available algorithms')
-
     def initialize_simulation(self):
         agents = Agents(
             agents_num=self.agents_num,
             positions=np.random.rand(self.agents_num, 2) * self.win_size,
-            # positions=np.array([
-            #     [100., 200.],
-            #     [400., 200.],
-            #     [200., 300.],
-            #     [400., 500.]
-            # ]),
             radiuses=np.full((self.agents_num, 1), 10),
             max_speeds=np.full((self.agents_num, 1), self.max_agents_speed),
             desired_speeds=np.full((self.agents_num, 1), 0.75 * self.max_agents_speed),
             velocity_diff_range=np.full((self.agents_num, 1), 10.)
         )
         targets = np.random.rand(self.agents_num, 2) * self.win_size
-        # targets = np.array([
-        #     [400., 500.],
-        #     [200., 500.],
-        #     [500., 400.],
-        #     [100., 200.]
-        # ])
         self.simulation.initialize(agents, targets)
 
-    def step(self, action):
-        self.time += self.time_step
+    def step(self, action: np.ndarray):
 
-        new_velocities = self.get_velocities()
-        # override 1st agent velocity with action
-        new_velocities[0] = action * self.max_agents_speed
-
-        self.simulation.agents.set_velocity(new_velocities)
-        self.simulation.agents.move(self.time_step)
+        if not self.dones[self.curr_agent_action]:
+            self.next_step_velocities[self.curr_agent_action] = action * self.max_agents_speed
 
         # observation
-        obs = self.get_observations()
-        # reward
-        dist = np.linalg.norm(self.simulation.agents.positions[0] - self.simulation.agents.targets[0])
-        reward = self.get_reward(distance=dist)
+        obs = self.get_observations(self.curr_agent_action)
 
-        # check if agent is still on scene
-        out_of_bounds = any(0 > self.simulation.agents.positions[0]) or \
-                        any(self.simulation.agents.positions[0] > self.win_size)
-        if out_of_bounds:
-            reward -= self.out_of_bounds_penalty
+        # current reward
+        curr_reward = self.rewards[self.curr_agent_action]
 
-        is_done = dist < np.finfo(float).eps or self.time > self.time_limit or out_of_bounds
+        # done
+        is_done = all(self.dones) or self.time > self.time_limit
+
+        if self.curr_agent_action == self.agents_num - 1:
+            self.time += self.time_step
+
+            new_velocities = self.next_step_velocities * self.max_agents_speed
+            self.simulation.agents.set_velocity(new_velocities)
+            self.simulation.agents.move(self.time_step)
+
+            # rewards
+            dist = np.linalg.norm(self.simulation.agents.positions - self.simulation.agents.targets, axis=1)
+            self.rewards = self.get_reward(distance=dist)
+
+            # check if agent is still on scene
+            out_of_bounds = (0 > self.simulation.agents.positions[:, 0]) | \
+                            (self.simulation.agents.positions[:, 0] > self.win_size[0]) | \
+                            (0 > self.simulation.agents.positions[:, 1]) | \
+                            (self.simulation.agents.positions[:, 1] > self.win_size[1])
+            self.rewards[out_of_bounds] -= self.out_of_bounds_penalty
+
+            self.dones = (dist < np.finfo(float).eps) | out_of_bounds
+
+        self.curr_agent_action = (self.curr_agent_action + 1) % self.agents_num
 
         # observation, reward, done, info
-        return obs, reward, is_done, {}
+        return obs, curr_reward, is_done, {}
 
     def reset(self):
         self.time = 0
+
+        self.curr_agent_action = 0
+        self.next_step_velocities = np.empty(shape=(self.agents_num, 2))
+        self.dones = np.full(shape=self.agents_num, fill_value=False, dtype=bool)
+        self.rewards = np.zeros(shape=self.agents_num)
+
         self.initialize_simulation()
-        distance = np.linalg.norm(self.simulation.agents.positions[0] - self.simulation.agents.targets[0])
+        distance = np.linalg.norm(self.simulation.agents.positions - self.simulation.agents.targets, axis=1)
         # get multiplicity of distance quantification
         self.closest_reached_distance = distance // self.distance_quantification * self.distance_quantification
-        return self.get_observations()
+        return self.get_observations(0)
 
     def render(self, mode='human', close=False):
         if mode == 'human':
@@ -176,46 +169,47 @@ class CollisionAvoidanceEnv(gym.Env, ABC):
         # pg.display.quit()
         super().close()
 
-    def get_observations(self):
-        nearest = self.simulation.agents.get_nearest_neighbours(self.visible_agents_num)[0]
+    def get_observations(self, agent_idx: int):
+        nearest = self.simulation.agents.get_nearest_neighbours(self.visible_agents_num)[agent_idx]
 
         visible_positions = self.simulation.agents.positions[nearest]
         visible_velocities = self.simulation.agents.velocities[nearest]
-        # assert all([all(a < b) for a, b in zip(visible_velocities, self.observation_space['velocity'].high)])
+
         return {
             'velocity_x': visible_velocities[:, 0],
             'velocity_y': visible_velocities[:, 1],
             'position_x': visible_positions[:, 0],
             'position_y': visible_positions[:, 1],
-            'target': self.simulation.agents.targets[0]
+            'target': self.simulation.agents.targets[agent_idx],
+            'self_position': self.simulation.agents.positions[agent_idx]
         }
 
     def get_reward(self, distance: float):
         nearest = self.simulation.agents.get_nearest_neighbours(1)
-        current_reward = -1  # time penalty
+        current_reward = np.full(self.agents_num, fill_value=-1.)  # time penalty
 
         # reward for reaching goal
-        if distance < np.finfo(np.float32).eps:
-            current_reward += self.goal_reward
+        current_reward[distance < np.finfo(np.float32).eps] += self.goal_reward
 
         # reward for shortening distance to target
-        if distance < self.closest_reached_distance:
-            self.closest_reached_distance -= self.distance_quantification
-            current_reward += self.distance_reward * self.distance_quantification
+        reached = distance < self.closest_reached_distance
+        self.closest_reached_distance[reached] -= self.distance_quantification
+        current_reward[reached] += self.distance_reward * self.distance_quantification
 
         # penalty for collisions
-        if self.simulation.is_colliding(0, nearest[0]):
-            current_reward -= self.collision_penalty
+        colliding = np.array([
+            self.simulation.is_colliding(i, nearest[i, 0])
+            for i, near in enumerate(nearest)
+        ])
+        current_reward[colliding] -= self.collision_penalty
 
         return current_reward
 
 
 if __name__ == '__main__':
-    env = CollisionAvoidanceEnv(
-        agents_num=20,
-        visible_agents_num=3,
+    env = MultiAgentCollisionAvoidanceEnv(
+        agents_num=4,
         max_speed=32.,
-        algorithm='VO',
         win_size=(600, 600)
     )
 
@@ -224,13 +218,13 @@ if __name__ == '__main__':
 
     episodes_num = 5
     for _ in range(episodes_num):  # episodes
-        done = False
+        done = np.full(env.agents_num, fill_value=False, dtype=bool)
         current_total_reward = 0
         observation = env.reset()
         t = 0
-        while not done:
+        while not all(done):
             env.render()
-            observation, reward, done, _ = env.step([0, 0])
+            observation, reward, done, _ = env.step(np.zeros(shape=(env.agents_num, 2), dtype=float))
             current_total_reward += reward
             t += 1
 
